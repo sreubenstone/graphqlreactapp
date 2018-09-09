2e3f3f362054e70eeef2e79689c3281f769439e0
Steven Reubenstone <Crossover@Stevens-MacBook-Air-3.local>
2018-09-09 13:28:08 -0400

    fixing

--- scratch.ts
+++ scratch.ts
@@ -1,17 +1,12 @@
-var pg = require("pg");
 require("dotenv").config();
-//or native libpq bindings
-//var pg = require('pg').native
-
-var conString =
-  process.env.CONNECTSTRING || "postgres://localhost:5432/graphqlhack";
-var client = new pg.Client(conString);
+const { Client } = require("pg");
 
 async () => {
+  const client = new Client();
   await client.connect();
-  return await client.query(
+  const res = await client.query(
     "INSERT INTO messages (message_body) VALUES ('young meat sandwich is good');"
   );
-  console.log(client.query);
-  client.end();
+  console.log(res);
+  await client.end();
 };
